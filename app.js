4a6c957183592e937d86e879a6abf309c7aa847b
Hadi Karaki <hadikaraki373@gmail.com>
2022-08-24 23:35:57 +0300

    'ad'

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ const connectSrcUrls = [
     "https://a.tiles.mapbox.com/",
     "https://b.tiles.mapbox.com/",
     "https://events.mapbox.com/",
-    "https://ajax.googleapis.com/"
+    "https://ajax.googleapis.com/",
     "http://e-comwebsite.herokuapp.com/"
 ];
 const fontSrcUrls = [
